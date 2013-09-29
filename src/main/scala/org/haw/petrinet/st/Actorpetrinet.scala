414b4a481b9c72df974e4e9a074d5e2a3953a255
Sigurd Sippel <sigurd.sippel@haw-hamburg.de>
2013-09-29 19:15:27 +0200

    extra examples

--- src/main/scala/org/haw/petrinet/st/Actorpetrinet.scala
+++ src/main/scala/org/haw/petrinet/st/Actorpetrinet.scala
@@ -5,23 +5,54 @@ import akka.actor.Props
 import com.typesafe.config.ConfigFactory
 
 object Actorpetrinet extends App {
-  val config = ConfigFactory.load() 
-  val system = ActorSystem("petrinet",config.getConfig("petrinet").withFallback(config))
-  val petrinetActor = system.actorOf(Props[PetrinetActor],"petrinetactor")
+  val config = ConfigFactory.load()
+  val system = ActorSystem("petrinet", config.getConfig("petrinet").withFallback(config))
+  val petrinetActor = system.actorOf(Props[PetrinetActor], "petrinetactor")
 
-  import Petrinet._
-  val p = P('p1, 'p2, 'p3)
-  val t = T('t1, 't2)
-  val e1 = 'p1 --> 't1 := 1
-  val e2 = 'p2 --> 't1 := 1
-  val e3 = 'p2 --> 't2 := 1
-  val e4 = 'p3 --> 't2 := 1
-  val e5 = 't1 <-- 'p2 := 1
-  val e6 = 't2 <-- 'p2 := 1
-  val e7 = 't2 <-- 'p3 := 1
-  val e8 = 't1 <-- 'p1 := 0
-  val w = W(e1, e2,e3,e4)(e5,e6,e7,e8)
-  val n = N(p, t, w)
+  val n1 = {
+    import Petrinet._
+    val p = P('p1, 'p2, 'p3)
+    val t = T('t1, 't2)
+    val e1 = 'p1 --> 't1 := 1
+    val e2 = 'p2 --> 't1 := 1
+    val e3 = 'p2 --> 't2 := 1
+    val e4 = 'p3 --> 't2 := 1
+    val e5 = 't1 <-- 'p2 := 1
+    val e6 = 't2 <-- 'p2 := 1
+    val e7 = 't2 <-- 'p3 := 1
+    val e8 = 't1 <-- 'p1 := 0
+    val w = W(e1, e2, e3, e4)(e5, e6, e7, e8)
+    N(p, t, w)
+  }
+  
+  val n2 = {
+    import Petrinet._
+    val p = P('p1, 'p2, 'p3, 'p4)
+    val t = T('t1, 't2)
+    val e1 = 'p1 --> 't1 := 1
+    val e2 = 'p2 --> 't1 := 1
+    val e3 = 'p2 --> 't2 := 1
+    val e4 = 'p3 --> 't2 := 1
+    val e5 = 't1 <-- 'p4 := 1
+    val e6 = 't2 <-- 'p4 := 1
+    val w = W(e1, e2, e3, e4)(e5, e6)
+    N(p, t, w)
+  }
+  
+  val n3 = {
+    import Petrinet._
+    val p = P('p1, 'p2, 'p3)
+    val t = T('t1, 't2)
+    val e1 = 'p1 --> 't1 := 1
+    val e2 = 't1 <-- 'p2 := 1
+    val e3 = 'p2 --> 't2 := 1
+    val e4 = 't2 <-- 'p1 := 1 
+    val w = W(e1, e3)(e2, e4)
+    N(p, t, w)
+  }
 
-  petrinetActor ! n
+  petrinetActor ! n3
+
 }
+
+
